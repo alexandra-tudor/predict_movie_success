01f236898cb22851bad997cf7eb415c77e9493cd
ralucagrigorescu <raluca_grigorescu@ymail.com>
2017-05-03 23:53:38 +0300

    correlation matrix without magical

--- src/plots/correlation_matrix.py
+++ src/plots/correlation_matrix.py
@@ -6,11 +6,19 @@ def print_correlation_matrix(df, size=10):
 	corr = df.corr()
 
 	fig, ax = plt.subplots(figsize=(size, size))
-	plt.title('Pearson Correlation of Movie Features')
+	#plt.title('Pearson Correlation of Movie Features')
 	ax.matshow(corr)
-	plt.xticks(range(len(corr.columns)), corr.columns)
+
+	print corr
+	#locs, labels = plt.xticks()
+
+	locs, labels = plt.xticks(range(len(corr.columns)), corr.columns)
+	plt.setp(labels, rotation=90)
+
+	#print range(len(corr.columns)), corr.columns
 	plt.yticks(range(len(corr.columns)), corr.columns)
+	plt.show()
 
 if __name__ == "__main__":
-	df_train = pd.read_csv('../../../data/sample_dataset.csv', sep='\t')
+	df_train = pd.read_csv('../../data/sample_dataset_no_magical.csv', sep='\t')
 	print_correlation_matrix(df_train)
