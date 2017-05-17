20f2d698df6f2a76b8f2da870dea56aee8e7778c
MihaelaGaman <mp.gaman@gmail.com>
2017-05-17 06:55:59 +0300

    Added code for performing sentiment analysis.

--- src/test/test_feature_extraction.py
+++ src/test/test_feature_extraction.py
@@ -1,3 +1,3 @@
 from src.features import complex_features
 
-print (complex_features.person_academy_awards('George Bancroft', 2017))
+print (complex_features.person_academy_awards('Tom Cruise', 2017))
